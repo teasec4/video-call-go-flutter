73fba21a9cffed2f7525ae8665183ad05b080691
Kovalev Maksim <teasec4@yandex.ru>
2026-01-21 10:34:17 +0800

    broadcast for clients, sending msg via node js console

---
+++ backend/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+type CLient struct{
+	conn *websocket.Conn
+	id string
+}
+
+var clients = make(map[*CLient]bool)
+var broadcast = make(chan []byte)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func main() {
+	go broadcastMessages()
+
+	http.HandleFunc("/ws", handleWebSocket)
+
+	fmt.Println("Server starting on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("WebSocket upgrade error:", err)
+		return
+	}
+	defer conn.Close()
+
+	client := &CLient{
+		conn: conn,
+		id: r.RemoteAddr,
+	}
+	clients[client] = true
+	log.Println("Client connected. Total clients:", len(clients))
+
+	defer func() {
+		delete(clients, client)
+		log.Println("client disconnected:", client.id)
+	}()
+	
+	for {
+		var msg []byte
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			delete(clients, client)
+			log.Println("Client disconnected. Total clients:", len(clients))
+			break
+		}
+		broadcast <- msg
+	}
+}
+
+func broadcastMessages() {
+	for msg := range broadcast {
+		for client := range clients {
+			err := client.conn.WriteMessage(websocket.TextMessage, msg)
+			if err != nil {
+				delete(clients, client)
+				client.conn.Close()
+			}
+		}
+	}
+}
--- backend/test.js
+++ backend/test.js
@@ -1,12 +1,21 @@
 const WebSocket = require("ws")
 
+const id = process.argv[2] || "anon";
 const ws = new WebSocket("ws://localhost:8080/ws")
 
 ws.on("open", () => {
-  console.log("connected");
-  ws.send("hello from node");
+  console.log(`[${id}] connected`);
 });
 
 ws.on("message", (data) => {
-  console.log("from server:", data.toString());
+  console.log(`[${id}] got:`, data.toString());
+});
+
+// manuall sending
+process.stdin.on("data", (data) => {
+  ws.send(JSON.stringify({
+    type: "chat",
+    from: id,
+    payload: data.toString().trim()
+  }));
 });
